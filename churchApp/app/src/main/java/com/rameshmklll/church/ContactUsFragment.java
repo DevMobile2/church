250cd24a98e2094457de9ec8377eee4f4e851700
ramesh <ramesh.mukalla@amzurtech.com>
2017-12-22 10:27:01 +0530

    new ui changes

--- churchApp/app/src/main/java/com/rameshmklll/church/ContactUsFragment.java
+++ churchApp/app/src/main/java/com/rameshmklll/church/ContactUsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +22,13 @@ public class ContactUsFragment extends Fragment {
 
     public ContactUsFragment(){
 
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Contact");
     }
 
     @Nullable
--- churchApp/app/src/main/java/com/rameshmklll/church/DashBoard.java
+++ churchApp/app/src/main/java/com/rameshmklll/church/DashBoard.java
@@ -192,6 +192,11 @@ public class DashBoard extends AppCompatActivity
             fragmentManager.beginTransaction().replace(R.id.frame_container, fragment, "Gallery")
                     .addToBackStack("Gallery").commit();
         }
+        else if (id == R.id.almanac) {
+            fragment=new AlmanacFragment();
+            fragmentManager.beginTransaction().replace(R.id.frame_container, fragment, "Almanac")
+                    .addToBackStack("Gallery").commit();
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
